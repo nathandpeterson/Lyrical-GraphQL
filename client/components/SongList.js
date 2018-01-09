c6e28ef40629ca20888c90340f12cc76754bcbfe
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-09 11:49:58 -0800

    add CreateSong component and connect to db

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 
 class SongList extends Component {
+    renderSongs(){
+        return this.props.data.songs.map((song, i) => {
+            return <li className="collection-item" key={i}>{song.title}</li>
+        })
+    }
 
     render() {
-        return <div>SongList</div>
+    if(this.props.data.loading) return <div>Loading</div>
+    return <div>
+         <ul className="collection">
+             {this.renderSongs()}
+         </ul>
+         <Link to={'/songs/new'}
+                className="btn-floating btn-large red right">
+                <i className="material-icons">add</i>
+         </Link>
+         </div>
     }
 }
 
-export default SongList
+const query = gql`
+{ 
+    songs 
+    {
+        title
+    }
+}`
+
+export default graphql(query)(SongList)
--- client/index.js
+++ client/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
-
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import App from './components/App'
 import SongList from './components/SongList'
+import SongCreate from './components/SongCreate'
 
 const client = new ApolloClient({})
 
 const Root = () => {
   return (
     <ApolloProvider client={ client }>
-        <SongList />
+      <Router history={ hashHistory }>
+        <Route path="/" components={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate}/>
+        </Route>
+      </Router>
     </ApolloProvider>
   )}
 
