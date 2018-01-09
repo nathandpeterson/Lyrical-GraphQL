7eb4ffb7ea5a0a912a0ec935e904c07c76bce77b
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-09 10:02:37 -0800

    setup apollo client and add SongList component

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://nathan:why7979@ds127892.mlab.com:27892/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
